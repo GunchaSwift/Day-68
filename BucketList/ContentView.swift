fa22f6737201c7f701c316d50d11f4b6e560d452
GunchaPy <guntars.reiss@gmail.com>
2022-09-24 12:02:57 +0300

    Writing data to the documents directory Switching states with enums

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,10 +7,62 @@
 
 import SwiftUI
 
-struct ContentView: View {
+extension FileManager {
+    func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+
+        // just send back the first one, which ought to be the only one
+        return paths[0]
+    }
+    func challengeTask(textString: String, fileName: String) {
+        let str = textString
+        let url = getDocumentsDirectory().appendingPathComponent(fileName)
+        
+        do {
+            try str.write(to: url, atomically: true, encoding: .utf8)
+            let input = try String(contentsOf: url)
+            print("Text string: \(input)")
+            print("File name: \(fileName)")
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+enum LoadingState {
+    case loading, success, failed
+}
+
+struct LoadingView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Loading...")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed.")
+    }
+}
+
+struct ContentView: View {
+    var loadingState = LoadingState.loading
+    
+    var body: some View {
+        if loadingState == .loading {
+            LoadingView()
+        } else if loadingState == .success {
+            SuccessView()
+        } else if loadingState == .failed {
+            FailedView()
+        }
     }
 }
 
